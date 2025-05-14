1adb3de529d07106f22e6fb3d24af1dd9ae15b2f
Harsha-Reddy <43820092+HarshaReddyVardhan@users.noreply.github.com>
2025-05-14 12:15:30 -0500

    Update Exercise_1.cpp

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,3 +1,9 @@
+// Time Complexity	O(log n)
+// Space Complexity	O(log n) (due to recursion stack)
+// Best Case	O(1) (if the element is at the middle)
+// Worst Case	O(log n)
+// Average Case	O(log n)
+
 #include <stdio.h> 
   
 // A recursive binary search function. It returns 
@@ -6,6 +12,15 @@
 int binarySearch(int arr[], int l, int r, int x) 
 {   
     //Your Code here 
+        if(l > r)
+            return -1;
+        int mid  = l + (r-l)/2;
+        if(arr[mid] == x)
+            return mid;
+        else if(arr[mid] > x)
+            return binarySearch(arr,l,mid-1,x);
+        else
+           return binarySearch(arr,mid+1,r,x);
 } 
   
 int main(void) 
