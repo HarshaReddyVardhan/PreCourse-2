25d4daa1a8b401bddd84ea75ebed5befc1904ded
Harsha-Reddy <43820092+HarshaReddyVardhan@users.noreply.github.com>
2025-07-09 23:42:37 -0500

    Update Exercise_5.cpp

--- Exercise_5.cpp
+++ Exercise_5.cpp
@@ -12,16 +12,57 @@ void swap(int* a, int* b)
 /* This function is same in both iterative and recursive*/
 int partition(int arr[], int l, int h) 
 { 
-    //Do the comparison and swapping here 
-} 
+    int pivot = arr[h]; 
+    int i = l - 1; // index of smaller element
+
+    for (int j = l; j <= h - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[h]);
+    return (i + 1);
+}
+
   
 /* A[] --> Array to be sorted,  
 l --> Starting index,  
 h --> Ending index */
 void quickSortIterative(int arr[], int l, int h) 
 { 
-    //Try to think that how you can use stack here to remove recursion.
-} 
+    // Create auxiliary stack
+    stack<int> s;
+
+    // Push initial values
+    s.push(l);
+    s.push(h);
+
+    // Keep popping until stack is empty
+    while (!s.empty()) {
+        // Pop h and l
+        h = s.top();
+        s.pop();
+        l = s.top();
+        s.pop();
+
+        // Partition the array
+        int p = partition(arr, l, h);
+
+        // If elements on the left of pivot, push left subarray
+        if (p - 1 > l) {
+            s.push(l);
+            s.push(p - 1);
+        }
+
+        // If elements on the right of pivot, push right subarray
+        if (p + 1 < h) {
+            s.push(p + 1);
+            s.push(h);
+        }
+    }
+}
+
   
 // A utility function to print contents of arr 
 void printArr(int arr[], int n) 
